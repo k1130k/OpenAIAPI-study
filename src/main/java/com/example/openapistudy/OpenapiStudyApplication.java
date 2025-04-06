09f22ee69bf1e8dc0d5dff51862a546c2c045d8a
k1130k <whdgns1130@naver.com>
2025-04-06 13:58:03 +0900

    Initial commit

---
+++ src/main/java/com/example/openapistudy/OpenapiStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.openapistudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenapiStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenapiStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/openapistudy/OpenapiStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.openapistudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapiStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
